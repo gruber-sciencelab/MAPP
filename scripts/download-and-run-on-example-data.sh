0abe8360b18a52e8580e5b703ff3ea62d7eae6e3
Maciej Bak <maciej.bak@unibas.ch>
2022-04-29 20:32:14 +0200

    adjust testscript for macOS

--- scripts/download-and-run-on-example-data.sh
+++ scripts/download-and-run-on-example-data.sh
@@ -50,28 +50,61 @@ rm -rf MAPP_test_data.tar.gz
 echo "### Adjusting configuration template ###"
 MAPP_directory=$(pwd)
 
-# Specify absolute paths to all the test data:
-sed -i '23s|""|"'$MAPP_directory'"|'  MAPP_test_data/config_template.yml
-genomic_annotation=${MAPP_directory}/MAPP_test_data/Homo_sapiens.GRCh38.87.chr21.gtf
-sed -i '26s|""|"'$genomic_annotation'"|'  MAPP_test_data/config_template.yml
-genomic_sequence=${MAPP_directory}/MAPP_test_data/Homo_sapiens.GRCh38.dna.chromosome.21.fa
-sed -i '35s|""|"'$genomic_sequence'"|'  MAPP_test_data/config_template.yml
-genomic_index=${MAPP_directory}/MAPP_test_data/GRCh38_STAR_index_hsa105
-sed -i '41s|""|"'$genomic_index'"|'  MAPP_test_data/config_template.yml
-analysis_design_table=${MAPP_directory}/MAPP_test_data/design_table.tsv
-sed -i '44s|""|"'$analysis_design_table'"|'  MAPP_test_data/config_template.yml
-PWM_directory=${MAPP_directory}/MAPP_test_data/PWMS
-sed -i '48s|""|"'$PWM_directory'"|'  MAPP_test_data/config_template.yml
-seqlogo_directory=${MAPP_directory}/MAPP_test_data/SEQLOGOS
-sed -i '52s|""|"'$seqlogo_directory'"|'  MAPP_test_data/config_template.yml
-PAS_atlas=${MAPP_directory}/MAPP_test_data/polyAsiteAtlas2.chr21.bed
-sed -i '55s|""|"'$PAS_atlas'"|'  MAPP_test_data/config_template.yml
+# Linux version
+if [ "$(uname)" == "Linux" ]; then
 
-#echo "### Building conda env for MAPP workflow ###"
-source $CONDA_PREFIX/etc/profile.d/conda.sh
-conda env remove --name mapp
-bash scripts/create-conda-environment.sh
-conda activate mapp
+    # Specify absolute paths to all the test data:
+    sed -i '23s|""|"'$MAPP_directory'"|'  MAPP_test_data/config_template.yml
+    genomic_annotation=${MAPP_directory}/MAPP_test_data/Homo_sapiens.GRCh38.87.chr21.gtf
+    sed -i '26s|""|"'$genomic_annotation'"|'  MAPP_test_data/config_template.yml
+    genomic_sequence=${MAPP_directory}/MAPP_test_data/Homo_sapiens.GRCh38.dna.chromosome.21.fa
+    sed -i '35s|""|"'$genomic_sequence'"|'  MAPP_test_data/config_template.yml
+    genomic_index=${MAPP_directory}/MAPP_test_data/GRCh38_STAR_index_hsa105
+    sed -i '41s|""|"'$genomic_index'"|'  MAPP_test_data/config_template.yml
+    analysis_design_table=${MAPP_directory}/MAPP_test_data/design_table.tsv
+    sed -i '44s|""|"'$analysis_design_table'"|'  MAPP_test_data/config_template.yml
+    PWM_directory=${MAPP_directory}/MAPP_test_data/PWMS
+    sed -i '48s|""|"'$PWM_directory'"|'  MAPP_test_data/config_template.yml
+    seqlogo_directory=${MAPP_directory}/MAPP_test_data/SEQLOGOS
+    sed -i '52s|""|"'$seqlogo_directory'"|'  MAPP_test_data/config_template.yml
+    PAS_atlas=${MAPP_directory}/MAPP_test_data/polyAsiteAtlas2.chr21.bed
+    sed -i '55s|""|"'$PAS_atlas'"|'  MAPP_test_data/config_template.yml
+
+    #echo "### Building conda env for MAPP workflow ###"
+    source $CONDA_PREFIX/etc/profile.d/conda.sh
+    conda env remove --name mapp
+    bash scripts/create-conda-environment.sh
+    conda activate mapp
+
+fi
+
+# Mac version
+if [ "$(uname)" == "Darwin" ]; then
+
+    # Specify absolute paths to all the test data:
+    sed -i '' -e '23s|""|"'$MAPP_directory'"|'  MAPP_test_data/config_template.yml
+    genomic_annotation=${MAPP_directory}/MAPP_test_data/Homo_sapiens.GRCh38.87.chr21.gtf
+    sed -i '' -e '26s|""|"'$genomic_annotation'"|'  MAPP_test_data/config_template.yml
+    genomic_sequence=${MAPP_directory}/MAPP_test_data/Homo_sapiens.GRCh38.dna.chromosome.21.fa
+    sed -i '' -e '35s|""|"'$genomic_sequence'"|'  MAPP_test_data/config_template.yml
+    genomic_index=${MAPP_directory}/MAPP_test_data/GRCh38_STAR_index_hsa105
+    sed -i '' -e '41s|""|"'$genomic_index'"|'  MAPP_test_data/config_template.yml
+    analysis_design_table=${MAPP_directory}/MAPP_test_data/design_table.tsv
+    sed -i '' -e '44s|""|"'$analysis_design_table'"|'  MAPP_test_data/config_template.yml
+    PWM_directory=${MAPP_directory}/MAPP_test_data/PWMS
+    sed -i '' -e '48s|""|"'$PWM_directory'"|'  MAPP_test_data/config_template.yml
+    seqlogo_directory=${MAPP_directory}/MAPP_test_data/SEQLOGOS
+    sed -i '' -e '52s|""|"'$seqlogo_directory'"|'  MAPP_test_data/config_template.yml
+    PAS_atlas=${MAPP_directory}/MAPP_test_data/polyAsiteAtlas2.chr21.bed
+    sed -i '' -e '55s|""|"'$PAS_atlas'"|'  MAPP_test_data/config_template.yml
+
+    #echo "### Building conda env for MAPP workflow ###"
+    source $CONDA_PREFIX/etc/profile.d/conda.sh
+    conda env remove --name mapp
+    bash scripts/create-conda-environment.sh
+    conda activate mapp
+
+fi
 
 echo "### Setting up configuration file for MAPP workflow ###"
 python scripts/create-main-config-file.py \
